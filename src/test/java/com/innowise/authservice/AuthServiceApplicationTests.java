3654cdacd8b5284ce9a40befb5fc03e137536f2d
travka677 <danikkim19@gmail.com>
2026-03-27 14:26:21 +0300

    update AuthServiceApplicationTests

--- src/test/java/com/innowise/authservice/AuthServiceApplicationTests.java
+++ src/test/java/com/innowise/authservice/AuthServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.innowise.authservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AuthServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
